6801a116a6392e3d8cf7c7c93f2e536a0c7992b3
kiyo <i.mitsuki19@gmail.com>
2021-05-02 19:13:31 +0900

    - fix model class  - add data binding setting to build.gradle

--- app/src/main/java/com/example/visiblepractice/CurrentDate.kt
+++ app/src/main/java/com/example/visiblepractice/CurrentDate.kt
@@ -2,31 +2,4 @@ package com.example.visiblepractice
 
 import java.util.*
 
-class CurrentDate {
-    // 今日の日付
-    var currentDate: Calendar
-        get() {
-            return currentDate
-        }
-        set(value) {
-            this.currentDate = value
-        }
-
-    // 曜日
-    var date: String
-        get() {
-            return date
-        }
-        set(value) {
-            this.date = value
-        }
-
-    // 整形後の今日の日付
-    var formattedDate: String
-        get() {
-            return formattedDate
-        }
-        set(value) {
-            this.formattedDate = value
-        }
-}
+data class CurrentDate(var currentDate: Calendar, var date: String, var formattedDate: String)
